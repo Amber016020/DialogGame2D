39bd55d78bfca5bfa8fb832cc59f4ced583fa942
吳佳融 <a0981369120@gmail.com>
2021-05-09 20:57:55 +0800

    實現背包拖曳

---
+++ DialogGame2D/Assets/Inventory/InventoryScripts/ItemOnDrag.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemOnDrag : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    public Transform originalParent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+        transform.SetParent(transform.parent.parent);
+        transform.position = eventData.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(eventData.pointerCurrentRaycast.gameObject.name == "Item Image")
+        {
+            transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent.parent);
+            transform.position = eventData.pointerCurrentRaycast.gameObject.transform.parent.parent.position;
+            eventData.pointerCurrentRaycast.gameObject.transform.parent.position = originalParent.position;
+            eventData.pointerCurrentRaycast.gameObject.transform.parent.SetParent(originalParent);
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+            return;
+        }
+        transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
+        transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
